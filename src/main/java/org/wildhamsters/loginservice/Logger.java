3f771a1ebdb8fedab14f898f8a4fedc87ec7cbe8
KamKoz92 <kamilkoziol92@gmail.com>
2022-06-10 08:27:50 +0200

    hothotfix: connection between microservices:

--- src/main/java/org/wildhamsters/loginservice/Logger.java
+++ src/main/java/org/wildhamsters/loginservice/Logger.java
@@ -6,9 +6,9 @@ import org.springframework.web.client.RestTemplate;
 
 class Logger {
     static void log(Log.Level level, Class<?> className, String msg) {
-        RestTemplate restTemplate = new RestTemplate();
-        Log log = new Log(level.toString(), LocalDateTime.now().toString(),
-                "battleShips", className.getName(), msg);
-        restTemplate.postForObject("http://64.225.104.111:8000/log", log, Log.class);
+        // RestTemplate restTemplate = new RestTemplate();
+        // Log log = new Log(level.toString(), LocalDateTime.now().toString(),
+        //         "battleShips", className.getName(), msg);
+        // restTemplate.postForObject("http://64.225.104.111:8000/log", log, Log.class);
     }
 }
